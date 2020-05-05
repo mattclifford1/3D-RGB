9bafd7a9fb1ccda43b469b0400d8a467478efd0f
Matt Clifford <matt@Matts-MacBook.local>
2020-05-05 16:27:32 +0100

    functional format --tested

--- 3d-inpainting/est_depth.py
+++ 3d-inpainting/est_depth.py
@@ -23,54 +23,40 @@ from bilateral_filtering import sparse_bilateral_filtering
 
 import utils_extra
 
-clock = utils_extra.timer()
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--config', type=str, default='argument.yml',help='Configure of post processing')
-args = parser.parse_args()
-config = yaml.load(open(args.config, 'r'))
+def run_samples(samples):
+    clock = utils_extra.timer()
 
-samples = utils_extra.im_data(config['src_folder'],
-                              config['depth_folder'],
-                              config['mesh_folder'])
+    if isinstance(config["gpu_ids"], int) and (config["gpu_ids"] >= 0):
+        device = config["gpu_ids"]
+    else:
+        device = "cpu"
 
-if isinstance(config["gpu_ids"], int) and (config["gpu_ids"] >= 0):
-    device = config["gpu_ids"]
-else:
-    device = "cpu"
+    print("Running on Device: {device}")
+    print("Set up time: " + clock.run_time())
 
-print("Running on Device: {device}")
-print("Set up time: " + clock.run_time())
+    for idx in range(samples.num_ims):
+        print(samples.im_file[idx])
+        depth = run_depth(samples.im_file[idx],
+                          config['src_folder'],
+                          config['depth_folder'],
+                          config['MiDaS_model_ckpt'],
+                          MonoDepthNet,
+                          MiDaS_utils,
+                          target_w=640.0)
+        np.save(samples.depth_file[idx], depth)
 
-for idx in range(samples.num_ims):
-    print(samples.im_file[idx])
-    depth = run_depth(samples.im_file[idx],
-                      config['src_folder'],
-                      config['depth_folder'],
-                      config['MiDaS_model_ckpt'],
-                      MonoDepthNet,
-                      MiDaS_utils,
-                      target_w=640.0)
-    np.save(samples.depth_file[idx], depth)
-
-    print("Depth estimated in: " + clock.run_time())
+        print("Depth estimated in: " + clock.run_time())
 
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default='argument.yml',help='Configure of post processing')
+    args = parser.parse_args()
+    config = yaml.load(open(args.config, 'r'))
 
-    # # get depth dims
-    # config['output_h'], config['output_w'] = np.load(sample['depth_fi']).shape[:2]
-    # # work out how to best rescale
-    # frac = config['longer_side_len'] / max(config['output_h'], config['output_w'])
-    # config['output_h'], config['output_w'] = int(config['output_h'] * frac), int(config['output_w'] * frac)
-    # config['original_h'], config['original_w'] = config['output_h'], config['output_w']
-    # # check if greyscale
-    # if image.ndim == 2:
-    #     image = image[..., None].repeat(3, -1)
-    # if np.sum(np.abs(image[..., 0] - image[..., 1])) == 0 and np.sum(np.abs(image[..., 1] - image[..., 2])) == 0:
-    #     config['gray_image'] = True
-    # else:
-    #     config['gray_image'] = False
-    # # resize image to depth size
-    # image = cv2.resize(image, (config['output_w'], config['output_h']), interpolation=cv2.INTER_AREA)
-    # # read depth
-    # depth = read_MiDaS_depth(sample['depth_fi'], 3.0, config['output_h'], config['output_w'])
+    samples = utils_extra.im_data(config['src_folder'],
+                                  config['depth_folder'],
+                                  config['mesh_folder'])
+
+    run_samples(samples)
