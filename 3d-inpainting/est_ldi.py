9a3eaa9833cd0f2247408e86ec88de3954807a37
Matt Clifford <matt@Matts-MacBook.local>
2020-05-15 15:27:25 +0100

    clean up ldi, not tested

--- 3d-inpainting/est_ldi.py
+++ 3d-inpainting/est_ldi.py
@@ -24,14 +24,42 @@ from bilateral_filtering import sparse_bilateral_filtering
 
 import utils_extra
 
+def load_models(config):
+    device = utils_extra.set_device(config)
+    torch.cuda.empty_cache()
+    depth_edge_model = Inpaint_Edge_Net(init_weights=True)
+    depth_edge_weight = torch.load(config['depth_edge_model_ckpt'],
+                                   map_location=torch.device(device))
+    depth_edge_model.load_state_dict(depth_edge_weight)
+    depth_edge_model = depth_edge_model.to(device)
+    depth_edge_model.eval()
+    if config['verbose']:
+        print("Loaded edge model in: " + clock.run_time())
+    depth_feat_model = Inpaint_Depth_Net()
+    depth_feat_weight = torch.load(config['depth_feat_model_ckpt'],
+                                   map_location=torch.device(device))
+    depth_feat_model.load_state_dict(depth_feat_weight, strict=True)
+    depth_feat_model = depth_feat_model.to(device)
+    depth_feat_model.eval()
+    depth_feat_model = depth_feat_model.to(device)
+    if config['verbose']:
+        print("Loaded depth model in: " + clock.run_time())
+    rgb_model = Inpaint_Color_Net()
+    rgb_feat_weight = torch.load(config['rgb_feat_model_ckpt'],
+                                 map_location=torch.device(device))
+    rgb_model.load_state_dict(rgb_feat_weight)
+    rgb_model.eval()
+    rgb_model = rgb_model.to(device)
+    return rgb_model, depth_edge_model, depth_feat_model
+
+
 def run_samples(samples, config):
     clock = utils_extra.timer()
-    device = utils_extra.set_device(config)
+    rgb_model, depth_edge_model, depth_feat_model = load_models(config)
     print('Estimating Frames...')
     for idx in tqdm(range(samples.data_num)):
         image = imageio.imread(samples.im_file[idx])
         int_mtx = utils_extra.int_mtx_CPY(image)
-
         config['output_h'], config['output_w'] = np.load(samples.depth_file[idx]).shape[:2]
         frac = config['longer_side_len'] / max(config['output_h'], config['output_w'])
         config['output_h'], config['output_w'] = int(config['output_h'] * frac), int(config['output_w'] * frac)
@@ -45,51 +73,26 @@ def run_samples(samples, config):
         image = cv2.resize(image, (config['output_w'], config['output_h']), interpolation=cv2.INTER_AREA)
         depth = read_MiDaS_depth(samples.depth_file[idx], 3.0, config['output_h'], config['output_w'])
         mean_loc_depth = depth[depth.shape[0]//2, depth.shape[1]//2]
-        if not(config['load_ply'] is True and os.path.exists(mesh_fi)):
-            vis_photos, vis_depths = sparse_bilateral_filtering(depth.copy(), image.copy(), config, num_iter=config['sparse_iter'], spdb=False)
-            depth = vis_depths[-1]
-            model = None
-            torch.cuda.empty_cache()
-            depth_edge_model = Inpaint_Edge_Net(init_weights=True)
-            depth_edge_weight = torch.load(config['depth_edge_model_ckpt'],
-                                           map_location=torch.device(device))
-            depth_edge_model.load_state_dict(depth_edge_weight)
-            depth_edge_model = depth_edge_model.to(device)
-            depth_edge_model.eval()
-            if config['verbose']:
-                print("Loaded edge model in: " + clock.run_time())
-            depth_feat_model = Inpaint_Depth_Net()
-            depth_feat_weight = torch.load(config['depth_feat_model_ckpt'],
-                                           map_location=torch.device(device))
-            depth_feat_model.load_state_dict(depth_feat_weight, strict=True)
-            depth_feat_model = depth_feat_model.to(device)
-            depth_feat_model.eval()
-            depth_feat_model = depth_feat_model.to(device)
-            if config['verbose']:
-                print("Loaded depth model in: " + clock.run_time())
-            rgb_model = Inpaint_Color_Net()
-            rgb_feat_weight = torch.load(config['rgb_feat_model_ckpt'],
-                                         map_location=torch.device(device))
-            rgb_model.load_state_dict(rgb_feat_weight)
-            rgb_model.eval()
-            rgb_model = rgb_model.to(device)
-            graph = None
-            if config['verbose']:
-                print("Loaded rgb model in: " + clock.run_time())
-                print("======= Starting LDI estimation ======")
-            rt_info = write_ply(image,
-                                  depth,
-                                  int_mtx,
-                                  samples.ldi_file[idx],
-                                  config,
-                                  rgb_model,
-                                  depth_edge_model,
-                                  depth_edge_model,
-                                  depth_feat_model)
-            torch.cuda.empty_cache()
-            if config['verbose']:
-                print("Estimated LDI in: " + clock.run_time())
-        print("Estimated frames in: " + clock.total_time())
+        vis_photos, vis_depths = sparse_bilateral_filtering(depth.copy(), image.copy(), config, num_iter=config['sparse_iter'], spdb=False)
+        depth = vis_depths[-1]
+        if config['verbose']:
+            print("Loaded rgb model in: " + clock.run_time())
+            print("======= Starting LDI estimation ======")
+        clock.reset()
+        rt_info = write_ply(image,
+                              depth,
+                              int_mtx,
+                              samples.ldi_file[idx],
+                              config,
+                              rgb_model,
+                              depth_edge_model,
+                              depth_edge_model,
+                              depth_feat_model)
+        print("Estimated LDI in: " + clock.run_time())
+        # torch.cuda.empty_cache()
+        if config['verbose']:
+            print("Estimated LDI in: " + clock.run_time())
+    print("Estimated frames in: " + clock.total_time())
 
 
 if __name__ == '__main__':
