89aee0a8f9dbba70c4e5ec44d23c692b37f0e0e9
Matt Clifford <matt@Matts-MacBook.local>
2020-05-15 22:51:57 +0100

    sh> exe

--- 3d-inpainting/RUN/bc4/submit_job.sh
+++ 3d-inpainting/RUN/bc4/submit_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -p gpu # Partition to submit to
 #SBATCH --gres=gpu:1
-# SBATCH --account=comsm0018       # use the course account
+#SBATCH --account=comsm0018       # use the course account
 
 module load module load languages/anaconda3/2019.07-3.6.5-tflow-1.14
 # you might want to install some modules (only need to do this once as --user saves to own envirnomnet)
