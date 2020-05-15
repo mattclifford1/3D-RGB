5a2ac32a4a54542da7a28bc05a4e1f830cf03e28
Matt Clifford <matt@Matts-MacBook.local>
2020-05-15 22:58:31 +0100

    fixed scrips

--- 3d-inpainting/RUN/bc4/submit_job.sh
+++ 3d-inpainting/RUN/bc4/submit_job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -p gpu # Partition to submit to
 #SBATCH --gres=gpu:1
-#SBATCH --account=comsm0018       # use the course account
+# SBATCH --account=comsm0018       # use the course account
 
-module load module load languages/anaconda3/2019.07-3.6.5-tflow-1.14
+module load languages/anaconda3/2019.07-3.6.5-tflow-1.14
 # you might want to install some modules (only need to do this once as --user saves to own envirnomnet)
 # pip install --user sklearn
 
