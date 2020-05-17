fb7c12d43291d6809968af2318f319ddfc8f1372
Matt Clifford <matt@Matts-MacBook.local>
2020-05-17 17:54:14 +0100

    osmesa use

--- 3d-inpainting/RUN/vid.sh
+++ 3d-inpainting/RUN/vid.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 source ~/.3D_env/bin/activate
+module load Mesa/11.2.1-foss-2016a
 
 echo "============ Making Video Sample ============"
 python construct_vid.py --config $1 --vid $2
--- 3d-inpainting/construct_vid.py
+++ 3d-inpainting/construct_vid.py
@@ -4,6 +4,7 @@ import glob
 import os
 from functools import partial
 import vispy
+vispy.use('osmesa')
 import scipy.misc as misc
 from tqdm import tqdm
 import yaml
