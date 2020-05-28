6fea77dc28335502c0bd2ec1cd97357570fabdad
Matt Clifford <matt@Matts-MacBook.local>
2020-05-28 21:26:49 +0100

    make mp4

--- 3d-inpainting/RUN/vid.sh
+++ 3d-inpainting/RUN/vid.sh
@@ -4,3 +4,6 @@ module load Mesa/11.2.1-foss-2016a
 
 echo "============ Making Video Sample ============"
 python construct_vid.py --config $1 --vid $2
+
+echo "============ VIDEO POSTPROCESSING ============"
+python process-data/postprocess_vid.py --config $1 --vid $2
